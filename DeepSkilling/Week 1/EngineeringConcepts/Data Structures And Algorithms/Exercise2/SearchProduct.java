a2e9c38915d98d1260ee3441dc4678e6310efb2d
Rishabh <rishabhdubey1472004@gmail.com>
2026-06-19 15:16:22 +0530

    dsa exercises done

---
+++ DeepSkilling/Week 1/EngineeringConcepts/Data Structures And Algorithms/Exercise2/SearchProduct.java
@@ -0,0 +1,11 @@
+public class SearchProduct {
+    int productId;
+    String productName;
+    String category;
+
+    public SearchProduct(int productId, String productName, String category) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+    }
+}
